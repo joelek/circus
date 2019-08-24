b5302ba90c9c5ebe4272cb30a25fa225531cd71a
Joel Ek <contact@joelek.se>
2019-08-25 00:07:18 +0200

    fixes

--- bmp.ts
+++ bmp.ts
@@ -1,36 +1,43 @@
 import * as libstream from "stream";
 
-function write_to(image: { w: number, h: number, indices: Buffer, palette: Buffer }, writable: libstream.Writable): void {
-	let stride = (((image.w + 3) >> 2) << 2);
+type Bitmap = {
+	w: number,
+	h: number,
+	buffer: Buffer,
+	palette: Buffer
+};
+
+function write_to(bitmap: Bitmap, writable: libstream.Writable): void {
+	let stride = (((bitmap.w + 3) >> 2) << 2);
 	let bmp_header = Buffer.alloc(14);
 	bmp_header.set(Buffer.from('BM', 'binary'), 0);
-	bmp_header.writeUInt32LE(14 + 40 + 256 * 4 + (stride * image.h), 2);
+	bmp_header.writeUInt32LE(14 + 40 + 256 * 4 + (stride * bitmap.h), 2);
 	bmp_header.writeUInt16LE(0, 6);
 	bmp_header.writeUInt16LE(0, 8);
 	bmp_header.writeUInt32LE(14 + 40 + 256 * 4, 10);
 	writable.write(bmp_header);
 	let dib_header = Buffer.alloc(40);
 	dib_header.writeUInt32LE(40, 0);
-	dib_header.writeUInt32LE(image.w, 4);
-	dib_header.writeUInt32LE(image.h, 8);
+	dib_header.writeUInt32LE(bitmap.w, 4);
+	dib_header.writeUInt32LE(bitmap.h, 8);
 	dib_header.writeUInt16LE(1, 12);
 	dib_header.writeUInt16LE(8, 14);
 	dib_header.writeUInt32LE(0, 16);
-	dib_header.writeUInt32LE((stride * image.h), 20);
+	dib_header.writeUInt32LE((stride * bitmap.h), 20);
 	dib_header.writeUInt32LE(2835, 24);
 	dib_header.writeUInt32LE(2835, 28);
 	dib_header.writeUInt32LE(0, 32);
 	dib_header.writeUInt32LE(0, 36);
 	writable.write(dib_header);
-	writable.write(image.palette);
-	let row = Buffer.alloc(stride);
-	for (let y = image.h - 1; y >= 0; y--) {
-		let offset = (y * image.w);
-		image.indices.copy(row, 0, offset, offset + image.w);
+	writable.write(bitmap.palette);
+	for (let y = bitmap.h - 1; y >= 0; y--) {
+		let row = Buffer.alloc(stride);
+		bitmap.buffer.copy(row, 0, (y * bitmap.w), (y * bitmap.w) + bitmap.w);
 		writable.write(row);
 	}
 }
 
-export = {
+export {
+	Bitmap,
 	write_to
 };
--- pgssub.ts
+++ pgssub.ts
@@ -1,35 +1,60 @@
-import * as libcp from 'child_process';
-import * as libcrypto from 'crypto';
+import * as bmp from './bmp';
 import * as libfs from 'fs';
-import * as libpath from 'path';
 
 function log(message: any): void {
 	console.log(message);
 }
 
-function parse_palette_block(chunk: { buffer: Buffer, offset: number }): void {
+type PaletteEntry = {
+	index: number;
+	y: number;
+	u: number;
+	v: number;
+	o: number;
+};
+
+function parse_palette_block(chunk: { buffer: Buffer, offset: number }): PaletteEntry {
 	let index = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
 	let y = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
-	let cr = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
-	let cb = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
-	let alpha = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
-	log({ index, y, cr, cb, alpha });
+	let u = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
+	let v = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
+	let o = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
+	return {
+		index,
+		y,
+		u,
+		v,
+		o
+	};
 }
 
-function parse_palette(chunk: { buffer: Buffer, offset: number }): void {
+type Palette = {
+	entries: Array<PaletteEntry>;
+};
+
+function parse_palette(chunk: { buffer: Buffer, offset: number }): Palette {
 	chunk.offset += 2;
 	let count = (chunk.buffer.length - 2) / 5;
+	let entries = new Array<PaletteEntry>(count);
 	for (let i = 0; i < count; i++) {
-		parse_palette_block(chunk);
+		entries[i] = parse_palette_block(chunk);
 	}
+	return {
+		entries
+	};
 }
 
-function parse_bitmap(chunk: { buffer: Buffer, offset: number }): void {
+type Bitmap = {
+	w: number,
+	h: number,
+	buffer: Buffer
+};
+
+function parse_bitmap(chunk: { buffer: Buffer, offset: number }): Bitmap {
 	chunk.offset += 7;
 	let w = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let h = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
-	log({ w, h });
-	let bmp = Buffer.alloc(w * h);
+	let buffer = Buffer.alloc(w * h);
 	let x = 0;
 	let y = 0;
 	while (y < h) {
@@ -56,69 +81,130 @@ function parse_bitmap(chunk: { buffer: Buffer, offset: number }): void {
 			}
 		}
 		for (let i = 0; i < count; i++) {
-			bmp.writeUInt8(index, (y * w) + x); x += 1;
+			buffer.writeUInt8(index, (y * w) + x); x += 1;
 		}
 	}
-	libfs.writeFileSync('../temp/test.raw', bmp);
+	return {
+		w,
+		h,
+		buffer
+	};
 }
 
-function parse_times_block(chunk: { buffer: Buffer, offset: number }): void {
-	chunk.offset += 3;
-	let forced = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
+type TimesEntry = {
+	x: number;
+	y: number;
+};
+
+function parse_times_block(chunk: { buffer: Buffer, offset: number }): TimesEntry {
+	chunk.offset += 4;
 	let x = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let y = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
-	log({ forced, x, y });
+	return {
+		x,
+		y
+	};
 }
 
-function parse_times(chunk: { buffer: Buffer, offset: number }): void {
+type Times = {
+	w: number;
+	h: number;
+	index: number;
+	entries: Array<TimesEntry>;
+};
+
+function parse_times(chunk: { buffer: Buffer, offset: number }): Times {
 	let w = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let h = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
-	let frame_rate = chunk.buffer.readInt8(chunk.offset); chunk.offset += 1;
+	chunk.offset += 1;
 	let index = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	chunk.offset += 3;
 	let count = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
-	log({ w, h, frame_rate, index, count });
+	let entries = new Array<TimesEntry>(count);
 	for (let i = 0; i < count; i++) {
-		parse_times_block(chunk);
+		entries[i] = parse_times_block(chunk);
 	}
+	return {
+		w,
+		h,
+		index,
+		entries
+	};
 }
 
-function parse_size_block(chunk: { buffer: Buffer, offset: number }): void {
+type SizeEntry = {
+	id: number,
+	x: number,
+	y: number,
+	w: number,
+	h: number
+};
+
+function parse_size_block(chunk: { buffer: Buffer, offset: number }): SizeEntry {
 	let id = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
 	let x = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let y = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let w = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 	let h = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
-	log({ id, x, y, w, h });
+	return {
+		id,
+		x,
+		y,
+		w,
+		h
+	};
 }
 
-function parse_size(chunk: { buffer: Buffer, offset: number }): void {
+type Size = {
+	entries: Array<SizeEntry>;
+};
+
+function parse_size(chunk: { buffer: Buffer, offset: number }): Size {
 	let count = chunk.buffer.readInt8(chunk.offset); chunk.offset += 1;
-	log({ count });
+	let entries = new Array<SizeEntry>(count);
 	for (let i = 0; i < count; i++) {
-		parse_size_block(chunk);
+		entries[i] = parse_size_block(chunk);
 	}
+	return {
+		entries
+	};
 }
 
 function parse_end(chunk: { buffer: Buffer, offset: number }): void {
 
 }
 
-function parse_pgssub(chunk: { buffer: Buffer, offset: number }): void {
+function parse_pgssub(chunk: { buffer: Buffer, offset: number }): bmp.Bitmap {
+	let palette = Buffer.alloc(256 * 4);
+	for (let i = 0; i < 256; i++) {
+		palette.writeUInt8(16, (i * 4) + 0);
+		palette.writeUInt8(0, (i * 4) + 1);
+		palette.writeUInt8(0, (i * 4) + 2);
+		palette.writeUInt8(255, (i * 4) + 3);
+	}
+	let bitmaps = new Array<Bitmap>(0);
+	let times = new Array<Times>(0);
+	let sizes = new Array<Size>(0);
 	while (true) {
 		let type = chunk.buffer.readUInt8(chunk.offset); chunk.offset += 1;
 		let size = chunk.buffer.readUInt16BE(chunk.offset); chunk.offset += 2;
 		let data = chunk.buffer.slice(chunk.offset, chunk.offset + size); chunk.offset += size;
-		log({ type, size });
 		if (false) {
 		} else if (type === 0x14) {
-			parse_palette({ buffer: data, offset: 0 });
+			let custom_palette = parse_palette({ buffer: data, offset: 0 });
+			for (let i = 0; i < custom_palette.entries.length; i++) {
+				let entry = custom_palette.entries[i];
+				palette.writeUInt8(entry.y, (entry.index * 4) + 0);
+				palette.writeUInt8(entry.u, (entry.index * 4) + 1);
+				palette.writeUInt8(entry.v, (entry.index * 4) + 2);
+				palette.writeUInt8(entry.o, (entry.index * 4) + 3);
+			}
 		} else if (type === 0x15) {
-			parse_bitmap({ buffer: data, offset: 0 });
+			bitmaps.push(parse_bitmap({ buffer: data, offset: 0 }));
 		} else if (type === 0x16) {
-			parse_times({ buffer: data, offset: 0 });
+			times.push(parse_times({ buffer: data, offset: 0 }));
 		} else if (type === 0x17) {
-			parse_size({ buffer: data, offset: 0 });
+			sizes.push(parse_size({ buffer: data, offset: 0 }));
 		} else if (type === 0x80) {
 			parse_end({ buffer: data, offset: 0 });
 			break;
@@ -126,6 +212,39 @@ function parse_pgssub(chunk: { buffer: Buffer, offset: number }): void {
 			throw new Error(``);
 		}
 	}
+	let w = 1920;
+	let h = 1080;
+	let buffer = Buffer.alloc(w * h);
+	buffer.fill(255);
+	for (let i = 0; i < bitmaps.length; i++) {
+		let bitmap = bitmaps[i];
+		let size = sizes[0].entries[0];
+		if (size) {
+			for (let y = 0; y < bitmap.h; y++) {
+				bitmap.buffer.copy(buffer, ((size.y + y) * w) + size.x, (y * size.w), (y * size.w) + size.w);
+			}
+		}
+	}
+	return {
+		w,
+		h,
+		buffer,
+		palette
+	};
 }
 
-parse_pgssub({ buffer: libfs.readFileSync(process.argv[2]), offset: 0 });
+let image = parse_pgssub({ buffer: libfs.readFileSync(process.argv[2]), offset: 0 });
+for (let i = 0; i < 256; i++) {
+	let y = image.palette.readUInt8((i * 4) + 0);
+	let u = image.palette.readUInt8((i * 4) + 1);
+	let v = image.palette.readUInt8((i * 4) + 2);
+	let o = image.palette.readUInt8((i * 4) + 3);
+	let k = ((y - 16) / (235 - 16) * o) | 0;
+	o = 255;
+	image.palette.writeUInt8(k, (i * 4) + 0);
+	image.palette.writeUInt8(k, (i * 4) + 1);
+	image.palette.writeUInt8(k, (i * 4) + 2);
+	image.palette.writeUInt8(o, (i * 4) + 3);
+}
+let writable = libfs.createWriteStream('../temp/test.bmp', { encoding: 'binary' });
+bmp.write_to(image, writable);
