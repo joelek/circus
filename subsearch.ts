bcd8db209194b77456dd55be3abae24271a5bbf4
Joel Ek <contact@joelek.se>
2019-11-09 23:03:39 +0100

    gif

--- subsearch.ts
+++ subsearch.ts
@@ -1,9 +1,11 @@
 import * as libfs from "fs";
+import * as libcp from "child_process";
 import * as libdatabase from "./database";
+import * as libutils from "./utils";
 
 function getSearchTerms(string: string): Array<string> {
 	let clean = string.toLowerCase().replace(/[^a-z ]/g, "").replace(/[ ]+/g, " "); // IMPROVE
-	let terms = clean.split(" ").filter((word) => word.length >= 3);
+	let terms = clean.split(" ").filter((word) => word.length >= 4);
 	return terms;
 }
 
@@ -64,6 +66,42 @@ if (cue_id_sets.length > 0) {
 		cue_ids.push(cue_id);
 	});
 }
-console.log(cue_ids.map((cue_id) => {
-	return media.video.cues.find((cue) => cue.cue_id === cue_id);
-}));
+if (cue_ids.length > 0) {
+	let cue_id = cue_ids[0];
+	let cue = media.video.cues.find((cue) => cue.cue_id === cue_id) as libdatabase.CueEntry;
+	let subtitle = media.video.subtitles.find((subtitle) => subtitle.subtitle_id === cue.subtitle_id) as libdatabase.SubtitleEntry;
+	let file_media = media.files.find((file) => file.file_id === subtitle.file_id) as libdatabase.FileEntry;
+	let episode: libdatabase.EpisodeEntry | null = null;
+	let file: libdatabase.FileEntry | null = null;
+	if (subtitle.episode_id !== null) {
+		episode = media.video.episodes.find((episode) => episode.episode_id === subtitle.episode_id) as libdatabase.EpisodeEntry;
+		file = media.files.find((file) => file.file_id === (episode as libdatabase.EpisodeEntry).file_id) as libdatabase.FileEntry;
+	}
+	let movie: libdatabase.MovieEntry | null = null;
+	if (subtitle.movie_id !== null) {
+		movie = media.video.movies.find((movie) => movie.movie_id === subtitle.movie_id) as libdatabase.MovieEntry;
+		file = media.files.find((file) => file.file_id === (movie as libdatabase.MovieEntry).file_id) as libdatabase.FileEntry;
+	}
+	process.stderr.write(JSON.stringify({
+		cue,
+		subtitle,
+		file,
+		episode,
+		movie
+	}, null, "\t"));
+	let cp = libcp.spawn("ffmpeg", [
+		"-ss", libutils.formatTimestamp(cue.start_ms),
+		"-t", libutils.formatTimestamp(cue.duration_ms),
+		"-i", [ ".", ...(file as libdatabase.FileEntry).path ].join("/"),
+		"-vf", libutils.join("subtitles=", [ ".", ...(file_media as libdatabase.FileEntry).path ].join("/")),
+		"test.gif",
+		"-y"
+	]);
+	process.stdin.pipe(cp.stdin);
+	cp.stdout.pipe(process.stdout);
+	cp.stderr.pipe(process.stderr);
+	cp.on("exit", () => {
+		process.exit();
+	});
+};
+//palettegen
--- utils.ts
+++ utils.ts
@@ -4,6 +4,21 @@ function join(...parameters: any): string {
 	}).join("");
 }
 
+function formatTimestamp(ms: number): string {
+	let s = Math.floor(ms / 1000);
+	ms -= (s * 1000);
+	let m = Math.floor(s / 60);
+	s -= (m * 60);
+	let h = Math.floor(m / 60);
+	m -= (h * 60);
+	let fh = join("00", h).slice(-2);
+	let fm = join("00", m).slice(-2);
+	let fs = join("00", s).slice(-2);
+	let fms = join("000", ms).slice(-3);
+	return join(fh, ":", fm, ":", fs, ".", fms);
+}
+
 export {
-	join
+	join,
+	formatTimestamp
 };
