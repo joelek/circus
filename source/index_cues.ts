671ea62ff66e3fac1bdb88ec61cf7f49d4fc0aae
Joel Ek <contact@joelek.se>
2020-07-30 23:23:58 +0200

    cueidfun

--- source/index_cues.ts
+++ source/index_cues.ts
@@ -5,15 +5,20 @@ import * as $fs from "fs";
 import * as $database from "./database";
 import * as $utils from "./utils";
 
+function getCueId(subtitle: $database.SubtitleEntry, cue: [number, number, string]): string {
+	let hash = $crypto.createHash("md5");
+	hash.update(subtitle.file_id);
+	hash.update("" + cue[0]);
+	let cue_id = hash.digest("hex");
+	return cue_id;
+}
+
 function run(): void {
 	let media = JSON.parse($fs.readFileSync("./private/db/media.json", "utf8")) as $database.MediaDatabase;
 	let index: { [key: string]: string[] | undefined } = {};
 	for (let subtitle of media.video.subtitles) {
 		for (let cue of subtitle.cues) {
-			let hash = $crypto.createHash("md5");
-			hash.update(subtitle.file_id);
-			hash.update("" + cue[0]);
-			let cue_id = hash.digest("hex");
+			let cue_id = getCueId(subtitle, cue);
 			for (let line of cue[2].split("\n")) {
 				let terms = $utils.getSearchTerms(line);
 				for (let term of terms) {
