f24bb694bca6218a6832711bf8906091e8fe5916
Joel Ek <contact@joelek.se>
2020-11-03 07:57:21 +0100

    Update probe.

--- source/probes/jpeg.ts
+++ source/probes/jpeg.ts
@@ -1,11 +1,38 @@
 import * as libfs from "fs";
 
-function readBuffer(fd: number, offset: number, buffer: Buffer): number {
-	let bytes = libfs.readSync(fd, buffer, 0, buffer.length, offset);
-	if (bytes !== buffer.length) {
-		throw `Expected to read ${buffer.length} bytes but read ${bytes}!`;
-	}
-	return offset + bytes;
+namespace readers {
+	export class Binary {
+		private fd: number;
+		private offset: number;
+
+		private read(buffer: Buffer): Buffer {
+			let bytes = libfs.readSync(this.fd, buffer, 0, buffer.length, this.offset);
+			if (bytes !== buffer.length) {
+				throw `Expected to read ${buffer.length} bytes but read ${bytes}!`;
+			}
+			this.offset += bytes;
+			return buffer;
+		}
+
+		private skip(length: number): void {
+			this.offset += length;
+		}
+
+		constructor(path: Array<string>) {
+			this.fd = libfs.openSync(path.join("/"), "r");
+			this.offset = 0;
+		}
+
+		newContext<A>(context: (read: (buffer: Buffer) => Buffer, skip: (length: number) => void) => A): A {
+			let offset = this.offset;
+			try {
+				return context((buffer) => this.read(buffer), (length) => this.skip(length));
+			} catch (error) {
+				this.offset = offset;
+				throw error;
+			}
+		}
+	};
 }
 
 enum Markers {
@@ -18,55 +45,98 @@ enum Markers {
 	DEFINE_HUFFMAN_TABLE = 0xFFC4,
 }
 
-const marker = Buffer.alloc(2);
-const length = Buffer.alloc(2);
+type StartOfImage = {
 
-function parseStartOfImage(fd: number, offset: number): number {
-	offset = readBuffer(fd, offset, marker);
-	if (marker.readUInt16BE(0) !== Markers.START_OF_IMAGE) {
-		throw ``;
-	}
-	return offset;
+};
+
+function parseStartOfImage(data: Buffer): StartOfImage {
+	return {};
 }
 
-function parseApplicationJfif(fd: number, offset: number): number {
-	offset = readBuffer(fd, offset, marker);
-	if (marker.readUInt16BE(0) !== Markers.APPLICATION_0) {
-		throw ``;
+type Application0 = {
+	major: number,
+	minor: number,
+	units: number,
+	x: number,
+	y: number,
+	w: number,
+	h: number,
+	preview: Buffer
+};
+
+function parseApplication0(data: Buffer): Application0 {
+	let offset = 0;
+	let identifier = data.slice(offset, 5); offset += 5;
+	if (identifier.toString() !== "JFIF\0") {
+		throw `Expected a JFIF tag!`;
 	}
-	offset = readBuffer(fd, offset, length);
-	let data = Buffer.alloc(length.readUInt16BE(0) - 2);
-	offset = readBuffer(fd, offset, data);
-	let identifier = data.slice(0, 5).toString();
-	if (identifier !== "JFIF\0") {
-		throw ``;
+	let major = data.readUInt8(offset); offset += 1;
+	let minor = data.readUInt8(offset); offset += 1;
+	let units = data.readUInt8(offset); offset += 1;
+	if ((units !== 0) && (units !== 1) && (units !== 2)) {
+		throw `Expected a valid density unit!`;
 	}
-	let major = data.readUInt8(5);
-	if (major !== 1) {
-		throw ``;
-	}
-	let minor = data.readUInt8(6);
-	if (minor !== 2) {
-		throw ``;
-	}
-	let units = data.readUInt8(7);
-	if (units !== 0 && units !== 1 && units !== 2) {
-		throw ``;
-	}
-	let x = data.readUInt16BE(8);
+	let x = data.readUInt16BE(offset); offset += 2;
 	if (x === 0) {
-		throw ``;
+		throw `Expected a non-zero horisontal resolution!`;
 	}
-	let y = data.readUInt16BE(10);
+	let y = data.readUInt16BE(offset); offset += 2;
 	if (y === 0) {
-		throw ``;
+		throw `Expected a non-zero vertical resolution!`;
 	}
-	let w = data.readUInt8(12);
-	let h = data.readUInt8(13);
-	if (w * h * 3 !== data.length - 14) {
-		throw ``;
+	let w = data.readUInt8(offset); offset += 1;
+	let h = data.readUInt8(offset); offset += 1;
+	let preview = data.slice(offset, offset + w * h * 3); offset += w * h * 3;
+	if (preview.length !== w * h * 3) {
+		throw `Expected a valid thumbnail!`;
 	}
-	return offset;
+	return {
+		major,
+		minor,
+		units,
+		x,
+		y,
+		w,
+		h,
+		preview
+	};
+}
+
+type StartOfFrame0 = {
+	precision: number,
+	height: number,
+	width: number,
+	component_count: number,
+	components: {
+		id: number,
+		sampling_factors: number,
+		quantization_table: number
+	}[]
+};
+
+function parseStartOfFrame0(data: Buffer): StartOfFrame0 {
+	let offset = 0;
+	let precision = data.readUInt8(offset); offset += 1;
+	let height = data.readUInt16BE(offset); offset += 2;
+	let width = data.readUInt16BE(offset); offset += 2;
+	let component_count = data.readUInt8(offset); offset += 1;
+	let components = [...Array(component_count).keys()].map(() => {
+		let id = data.readUInt8(offset); offset += 1;
+		let sampling_factors = data.readUInt8(offset); offset += 1;
+		let quantization_table = data.readUInt8(offset); offset += 1;
+		return {
+			id,
+			sampling_factors,
+			quantization_table
+		};
+	});
+	return {
+		precision,
+		height,
+		width,
+		component_count,
+		components
+	};
 }
 
 type ProbeResult = {
@@ -74,32 +144,33 @@ type ProbeResult = {
 	height: number
 };
 
-export function probe(fd: number): ProbeResult {
-	let offset = 0;
-	offset = parseStartOfImage(fd, offset);
-	offset = parseApplicationJfif(fd, offset);
-	while (true) {
-		offset = readBuffer(fd, offset, marker);
-		if (marker.readUInt16BE(0) === Markers.START_OF_FRAME_0) {
-			offset = readBuffer(fd, offset, length);
-			let data = Buffer.alloc(length.readUInt16BE(0) - 2);
-			offset = readBuffer(fd, offset, data);
-			let precision = data.readUInt8(0);
-			let height = data.readUInt16BE(1);
-			let width = data.readUInt16BE(3);
-			let component_count = data.readUInt8(5);
-			for (let i = 0; i < component_count; i++) {
-				let component_id = data.readUInt8(6 + i * 3 + 0);
-				let component_sampling_factors = data.readUInt8(6 + i * 3 + 1);
-				let component_quantization_table = data.readUInt8(6 + i * 3 + 2);
-			}
-			return {
-				width,
-				height
-			};
-		} else {
-			offset = readBuffer(fd, offset, length);
-			offset += length.readUInt16BE(0) - 2;
+export function probe(reader: readers.Binary): ProbeResult {
+	return reader.newContext((read, skip) => {
+		let marker = Buffer.alloc(2);
+		let length = Buffer.alloc(2);
+		read(marker);
+		if (marker.readUInt16BE() !== Markers.START_OF_IMAGE) {
+			throw `Expected SOI marker!`;
 		}
-	}
+		parseStartOfImage(Buffer.alloc(0));
+		read(marker);
+		if (marker.readUInt16BE() !== Markers.APPLICATION_0) {
+			throw `Expected APP0 marker!`;
+		}
+		read(length);
+		parseApplication0(read(Buffer.alloc(length.readUInt16BE() - 2)));
+		while (true) {
+			read(marker);
+			read(length);
+			if (marker.readUInt16BE() === Markers.START_OF_FRAME_0) {
+				let sof = parseStartOfFrame0(read(Buffer.alloc(length.readUInt16BE() - 2)));
+				return {
+					width: sof.width,
+					height: sof.height
+				};
+			} else {
+				skip(length.readUInt16BE() - 2);
+			}
+		}
+	});
 };
