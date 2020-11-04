1d1a156a058d823d260442c24fa38ff4de0ab084
Joel Ek <contact@joelek.se>
2020-11-04 21:46:16 +0100

    Export sorters.

--- source/indices/index.ts
+++ source/indices/index.ts
@@ -3,3 +3,6 @@ export * as sorters from "./sorters";
 export { CollectionIndex } from "./indices";
 export { RecordIndex } from "./indices";
 export { SearchIndex } from "./indices";
+export { CombinedSort } from "./sorters";
+export { LexicalSort } from "./sorters";
+export { NumericSort } from "./sorters";
