5ad3605b8bdf86781b6594f25304c6e14b5b4633
Joel Ek <contact@joelek.se>
2020-11-12 18:29:53 +0100

    Fix input selection.

--- source/client/client.ts
+++ source/client/client.ts
@@ -347,16 +347,6 @@ style.innerText = `
 		line-height: 1;
 		outline: none;
 		padding: 0px;
-		user-select: none;
-			-webkit-user-select: none;
-			-moz-user-select: none;
-			-ms-user-select: none;
-			-o-user-select: none;
-		touch-callout: none;
-			-webkit-touch-callout: none;
-			-moz-touch-callout: none;
-			-ms-touch-callout: none;
-			-o-touch-callout: none;
 	}
 
 	a,
@@ -413,6 +403,16 @@ style.innerText = `
 		color: rgb(255, 255, 255);
 		font-family: "Nunito", sans-serif;
 		overflow: hidden;
+		touch-callout: none;
+			-webkit-touch-callout: none;
+			-moz-touch-callout: none;
+			-ms-touch-callout: none;
+			-o-touch-callout: none;
+		user-select: none;
+			-webkit-user-select: none;
+			-moz-user-select: none;
+			-ms-user-select: none;
+			-o-user-select: none;
 	}
 
 	body ::selection {
