cb368ec20aa75af9b5099129af20ef4f3653b74d
Joel Ek <contact@joelek.se>
2021-03-10 23:10:36 +0100

    Update db.

--- source/jdb2/index.ts
+++ source/jdb2/index.ts
@@ -129,7 +129,7 @@ export class Entry extends Chunk {
 };
 
 export class BlockHandler {
-	static RESERVED_BLOCKS = 256;
+	static FIRST_APPLICATION_BLOCK = 256;
 
 	private bin: number;
 	private toc: number;
@@ -186,10 +186,14 @@ export class BlockHandler {
 		this.bin = open([ ...path, "bin" ]);
 		this.toc = open([ ...path, "toc" ]);
 		if (this.getCount() === 0) {
-			for (let i = 0; i < BlockHandler.RESERVED_BLOCKS; i++) {
+			for (let i = 0; i < BlockHandler.FIRST_APPLICATION_BLOCK; i++) {
 				this.createNewBlock(16);
 			}
 		}
+	}
+
+	clearBlock(index: number): void {
+		this.writeBlock(index);
 	}
 
 	createBlock(minLength: number): number {
@@ -202,7 +206,7 @@ export class BlockHandler {
 		throw `Unable to create block with length ${minLength}!`;
 	}
 
-	private deleteBlock(index: number): void {
+	deleteBlock(index: number): void {
 		let entry = new Entry();
 		this.readEntry(index, entry);
 		if (entry.deleted) {
@@ -227,177 +231,191 @@ export class BlockHandler {
 		this.writeEntry(index, entry);
 	}
 
+	getBlockSize(index: number): number {
+		let entry = new Entry();
+		this.readEntry(index, entry);
+		return entry.length;
+	}
+
 	getCount(): number {
 		let count = size(this.toc) / Entry.SIZE;
 		IntegerAssert.atLeast(0, count);
 		return count;
 	}
 
-	readBlock(index: number, buffer: Buffer, offset: number = 0): void {
-		let length = buffer.length;
+	readBlock(index: number, buffer?: Buffer, skipLength?: number): Buffer {
 		let entry = new Entry();
 		this.readEntry(index, entry);
+		buffer = buffer ?? Buffer.alloc(entry.length);
+		let offset = skipLength ?? 0;
+		let length = buffer.length;
 		IntegerAssert.between(0, offset, entry.length - 1);
 		IntegerAssert.between(0, length, entry.length - offset);
 		read(this.bin, buffer, entry.offset + offset);
+		return buffer;
 	}
 
-	resizeBlock(indexOne: number, minLength: number): void {
-		let entryOne = new Entry();
-		this.readEntry(indexOne, entryOne);
-		if (this.computePool(minLength) === this.computePool(entryOne.length)) {
+	resizeBlock(index: number, minLength: number): void {
+		let entry = new Entry();
+		this.readEntry(index, entry);
+		if (this.computePool(minLength) === this.computePool(entry.length)) {
 			return;
 		}
 		let indexTwo = this.createBlock(minLength);
 		let entryTwo = new Entry();
 		this.readEntry(indexTwo, entryTwo);
-		let length = Math.min(entryOne.length, entryTwo.length);
+		let length = Math.min(entry.length, entryTwo.length);
 		let buffer = Buffer.alloc(length);
-		read(this.bin, buffer, entryOne.offset);
+		read(this.bin, buffer, entry.offset);
 		write(this.bin, buffer, entryTwo.offset);
-		this.writeEntry(indexOne, entryTwo);
-		this.writeEntry(indexTwo, entryOne);
+		this.writeEntry(index, entryTwo);
+		this.writeEntry(indexTwo, entry);
 		this.deleteBlock(indexTwo);
 	}
 
-	writeBlock(index: number, buffer: Buffer, offset: number = 0): void {
-		let length = buffer.length;
+	swapBlocks(indexOne: number, indexTwo: number): void {
+		let entryOne = new Entry();
+		this.readEntry(indexOne, entryOne);
+		let entryTwo = new Entry();
+		this.readEntry(indexTwo, entryTwo);
+		this.writeEntry(indexOne, entryTwo);
+		this.writeEntry(indexTwo, entryOne);
+	}
+
+	writeBlock(index: number, buffer?: Buffer, skipLength?: number): Buffer {
 		let entry = new Entry();
 		this.readEntry(index, entry);
+		buffer = buffer ?? Buffer.alloc(entry.length);
+		let offset = skipLength ?? 0;
+		let length = buffer.length;
 		IntegerAssert.between(0, offset, entry.length - 1);
 		IntegerAssert.between(0, length, entry.length - offset);
 		write(this.bin, buffer, entry.offset + offset);
+		if (is.absent(skipLength)) {
+			write(this.bin, Buffer.alloc(entry.length - buffer.length), buffer.length);
+		}
+		return buffer
 	}
 };
 
-export type Keys = Array<boolean | null | number | string | undefined>;
+export type Keys = Array<string | undefined>;
 export type KeysProvider<A> = (record: A) => Keys;
-export type RecordDeserializer<A> = (buffer: Buffer) => A;
 export type RecordSerializer<A> = (record: A) => Buffer;
+export type RecordDeserializer<A> = (buffer: Buffer) => A;
+
+
+
 export type SearchResult<A> = {
 	record(): A;
 	records(includePrefixMatches?: boolean): Array<A>;
 };
 
-export class Node extends Chunk {
-	static SIZE = 32;
-
-	get nodeTableIndex(): number {
-		return this.buffer.readUInt32BE(4);
-	}
-
-	set nodeTableIndex(value: number) {
-		IntegerAssert.between(0, value, 0xFFFFFFFF);
-		this.buffer.writeUInt32BE(value, 4);
-	}
-
-	get exactMatchesIndex(): number {
-		return this.buffer.readUInt32BE(12);
-	}
-
-	set exactMatchesIndex(value: number) {
-		IntegerAssert.between(0, value, 0xFFFFFFFF);
-		this.buffer.writeUInt32BE(value, 12);
-	}
-
-	get prefixMatchesIndex(): number {
-		return this.buffer.readUInt32BE(20);
-	}
-
-	set prefixMatchesIndex(value: number) {
-		IntegerAssert.between(0, value, 0xFFFFFFFF);
-		this.buffer.writeUInt32BE(value, 20);
-	}
-
-	get nullIndex(): number {
-		return this.buffer.readUInt32BE(28);
-	}
-
-	set nullIndex(value: number) {
-		IntegerAssert.between(0, value, 0xFFFFFFFF);
-		this.buffer.writeUInt32BE(value, 28);
-	}
-
-	constructor(buffer?: Buffer) {
-		buffer = buffer ?? Buffer.alloc(Entry.SIZE);
-		IntegerAssert.exactly(buffer.length, Entry.SIZE);
-		super(buffer);
-	}
-};
-
 export class Table<A> {
 	private blockHandler: BlockHandler;
 	private keysProvider: KeysProvider<A>;
+	private recordSerializer: RecordSerializer<A>;
+	private recordDeserializer: RecordDeserializer<A>;
 
-	// Array of
-	private serializeKey(keys: Keys): Array<number | null | undefined> {
-		let buffers = keys
-			.map((key) => {
-				if (key === null) {
-					return null;
-				}
-				if (key === undefined) {
-					return Buffer.of(0);
-				}
-				if (key === false) {
-					return Buffer.of(0);
-				}
-				if (key === true) {
-					return Buffer.of(1);
-				}
-				// TODO: Improve number serialization.
-				if (typeof key === "number") {
-					let buffer = Buffer.of(8);
-					buffer.writeUInt32BE(key, 4);
-					return buffer;
-				}
-				if (typeof key === "string") {
-					return Buffer.from(key);
-				}
-			})
-			.filter(is.present);
-		throw ``;
+	private getKeyBytes(keys: Keys): Array<number | undefined> {
+		let bytes = new Array<number | undefined>();
+		for (let [index, key] of keys.entries()) {
+			if (index > 0) {
+				bytes.push(undefined);
+			}
+			if (typeof key === "string") {
+				let buffer = Buffer.from(key);
+				bytes.push(...buffer);
+			} else {
+				bytes.push(undefined);
+			}
+		}
+		return bytes;
 	}
 
-	private getNode(key: Buffer): Node {
-		let index = BlockHandler.RESERVED_BLOCKS + 1;
-
-/*
-three pointers to blocks
-1) first pointer is to block with fixed size table of 256 pointers = 2048 bytes
-2) second pointer is to block with exact matches
-3) third pointer is to block with prefix matches
-*/
-		throw ``;
+	private compareKeys(one: Keys, two: Keys): boolean {
+		if (one.length !== two.length) {
+			return false;
+		}
+		for (let i = 0; i < one.length; i++) {
+			if (one[i] !== two[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
-	constructor(blockHandler: BlockHandler, keysProvider: KeysProvider<A>) {
+	private getTableIndex(keyBytes: Array<number | undefined>, keyBytesIndex: number): number {
+		return keyBytesIndex < keyBytes.length ? keyBytes[keyBytesIndex] ?? 256 : 257;
+	}
+
+	constructor(blockHandler: BlockHandler, keysProvider: KeysProvider<A>, recordSerializer: RecordSerializer<A>, recordDeserializer: RecordDeserializer<A>) {
 		this.blockHandler = blockHandler;
 		this.keysProvider = keysProvider;
-		if (blockHandler.getCount() === BlockHandler.RESERVED_BLOCKS) {
-			blockHandler.createBlock(Node.SIZE);
+		this.recordSerializer = recordSerializer;
+		this.recordDeserializer = recordDeserializer;
+		if (blockHandler.getCount() === BlockHandler.FIRST_APPLICATION_BLOCK) {
+			blockHandler.createBlock(Pointer.SIZE);
 		}
 	}
 
 	delete(record: A): void {
-		let keys = this.keysProvider(record);
-		let key = this.serializeKey(keys);
+		throw ``;
 	}
 
 	insert(record: A): void {
+		let serializedRecord = this.recordSerializer(record);
 		let keys = this.keysProvider(record);
-		let key = this.serializeKey(keys);
+		let keyBytes = this.getKeyBytes(keys);
+		let keyBytesIndex = 0;
+		let nodeIndex = BlockHandler.FIRST_APPLICATION_BLOCK;
+		let pointer = new Pointer();
+		while (true) {
+			if (this.blockHandler.getBlockSize(nodeIndex) === Pointer.SIZE) {
+				this.blockHandler.readBlock(nodeIndex, pointer.buffer, 0);
+				if (pointer.index === 0) {
+					pointer.index = this.blockHandler.createBlock(serializedRecord.length);
+					this.blockHandler.writeBlock(pointer.index, serializedRecord);
+					this.blockHandler.writeBlock(nodeIndex, pointer.buffer, 0);
+					// TODO: Emit insert event.
+					break;
+				} else {
+					let recordTwo = this.recordDeserializer(this.blockHandler.readBlock(pointer.index));
+					let keysTwo = this.keysProvider(recordTwo);
+					if (this.compareKeys(keys, keysTwo)) {
+						this.blockHandler.resizeBlock(pointer.index, serializedRecord.length);
+						this.blockHandler.writeBlock(pointer.index, serializedRecord);
+						// TODO: Emit update event.
+						break;
+					} else {
+						let otherNodeIndex = this.blockHandler.createBlock(Pointer.SIZE * 256 + Pointer.SIZE + Pointer.SIZE);
+						this.blockHandler.writeBlock(otherNodeIndex, pointer.buffer);
+						this.blockHandler.swapBlocks(nodeIndex, otherNodeIndex);
+						let keyBytesTwo = this.getKeyBytes(keysTwo);
+						let tableIndex = this.getTableIndex(keyBytesTwo, keyBytesIndex);
+						pointer.index = otherNodeIndex;
+						this.blockHandler.writeBlock(nodeIndex, pointer.buffer, Pointer.SIZE * tableIndex);
+					}
+				}
+			}
+			let tableIndex = this.getTableIndex(keyBytes, keyBytesIndex);
+			keyBytesIndex += 1;
+			this.blockHandler.readBlock(nodeIndex, pointer.buffer, Pointer.SIZE * tableIndex);
+			if (pointer.index === 0) {
+				pointer.index = this.blockHandler.createBlock(Pointer.SIZE);
+				this.blockHandler.writeBlock(nodeIndex, pointer.buffer, Pointer.SIZE * tableIndex);
+			}
+			nodeIndex = pointer.index;
+		}
 	}
 
 	search(...keys: Keys): SearchResult<A> {
 		throw ``;
 	}
-
-	update(record: A): void {
-		let keys = this.keysProvider(record);
-		let key = this.serializeKey(keys);
-	}
 };
 
 let blockHandler = new BlockHandler([ ".", "private", "jdb2" ]);
-let table = new Table<Person>(blockHandler, (record) => [ record.person_id ]);
+let table = new Table<Person>(blockHandler, (record) => [ record.person_id ], (record) => Buffer.from(JSON.stringify(record)), (buffer) => Person.as(JSON.parse(Buffer.toString())));
+table.insert({
+	person_id: "a",
+	name: "Joel"
+});
