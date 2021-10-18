317b150ec1bef9404e50ce6338d36d6b65286c25
Joel Ek <contact@joelek.se>
2021-10-18 10:48:50 +0200

    Improve asserts.

--- source/jdb2/asserts/integer.ts
+++ source/jdb2/asserts/integer.ts
@@ -1,38 +1,47 @@
 export class IntegerAssert {
 	private constructor() {}
 
-	static atLeast(min: number, value: number): void {
+	static atLeast(min: number, value: number): number {
 		this.integer(min);
 		this.integer(value);
 		if (value < min) {
 			throw `Expected ${value} to be at least ${min}!`;
 		}
+		return value;
 	}
 
-	static atMost(max: number, value: number): void {
+	static atMost(max: number, value: number): number {
 		this.integer(value);
 		this.integer(max);
 		if (value > max) {
 			throw `Expected ${value} to be at most ${max}!`;
 		}
+		return value;
 	}
 
-	static between(min: number, value: number, max: number): void {
-		this.atLeast(min, value);
-		this.atMost(max, value);
+	static between(min: number, value: number, max: number): number {
+		this.integer(min);
+		this.integer(value);
+		this.integer(max);
+		if (value < min || value > max) {
+			throw `Expected ${value} to be between ${min} and ${max}!`;
+		}
+		return value;
 	}
 
-	static exactly(value: number, expected: number): void {
+	static exactly(value: number, expected: number): number {
 		this.integer(expected);
 		this.integer(value);
 		if (value !== expected) {
 			throw `Expected ${value} to be exactly ${expected}!`;
 		}
+		return value;
 	}
 
-	static integer(value: number): void {
+	static integer(value: number): number {
 		if (!Number.isInteger(value)) {
 			throw `Expected ${value} to be an integer!`;
 		}
+		return value;
 	}
 };
--- source/jdb2/asserts/string.ts
+++ source/jdb2/asserts/string.ts
@@ -1,9 +1,10 @@
 export class StringAssert {
 	private constructor() {}
 
-	static identical(value: string, expected: string): void {
+	static identical(value: string, expected: string): string {
 		if (value !== expected) {
 			throw `Expected "${value}" to be identical to ${expected}!`;
 		}
+		return value;
 	}
 };
