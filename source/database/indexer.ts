d336062ecc1149828ab989dbc340b5a058eda775
Joel Ek <contact@joelek.se>
2022-04-06 10:29:55 +0200

    Improve indexer.

--- source/database/indexer.ts
+++ source/database/indexer.ts
@@ -95,6 +95,7 @@ function getFilePath(queue: ReadableQueue, file: File): Promise<Array<string>> {
 
 async function checkFile(queue: WritableQueue, root: File, paths: Array<string>): Promise<void> {
 	let path = paths.join("/");
+	console.log(`Checking ${path}...`);
 	if (libfs.existsSync(path)) {
 		let stats = libfs.statSync(path);
 		if (stats.isFile()) {
@@ -109,6 +110,7 @@ async function checkFile(queue: WritableQueue, root: File, paths: Array<string>)
 
 async function checkDirectory(queue: WritableQueue, root: Directory, paths: Array<string>): Promise<void> {
 	let path = paths.join("/");
+	console.log(`Checking ${path}...`);
 	if (libfs.existsSync(path)) {
 		let stats = libfs.statSync(path);
 		if (stats.isDirectory()) {
@@ -178,15 +180,13 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 			show_id: show_id,
 			name: metadata.show.title,
 			summary: metadata.show.summary ?? null,
-			imdb: metadata.show.imdb ?? null,
-			timestamp_ms: null
+			imdb: metadata.show.imdb ?? null
 		});
 		let season_id = makeBinaryId("season", show_id, `${metadata.season}`);
 		await stores.seasons.update(queue, {
 			season_id: season_id,
 			show_id: show_id,
-			number: metadata.season,
-			timestamp_ms: null
+			number: metadata.season
 		});
 		let episode_id = makeBinaryId("episode", season_id, `${metadata.episode}`);
 		await stores.episodes.update(queue, {
@@ -197,8 +197,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 			year_id: year_id ?? null,
 			summary: metadata.summary ?? null,
 			copyright: metadata.copyright ?? null,
-			imdb: metadata.imdb ?? null,
-			timestamp_ms: null
+			imdb: metadata.imdb ?? null
 		});
 		for (let file_id of file_ids) {
 			await stores.episode_files.insert(queue, {
@@ -246,8 +245,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 			year_id: year_id ?? null,
 			summary: metadata.summary ?? null,
 			copyright: metadata.copyright ?? null,
-			imdb: metadata.imdb ?? null,
-			timestamp_ms: null
+			imdb: metadata.imdb ?? null
 		});
 		for (let file_id of file_ids) {
 			await stores.movie_files.insert(queue, {
@@ -292,8 +290,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 		await stores.albums.update(queue, {
 			album_id: album_id,
 			title: metadata.album.title,
-			year_id: year_id ?? null,
-			timestamp_ms: null
+			year_id: year_id ?? null
 		});
 		for (let [index, artist] of metadata.album.artists.entries()) {
 			let artist_id = makeBinaryId("artist", artist);
@@ -311,8 +308,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 		await stores.discs.update(queue, {
 			disc_id: disc_id,
 			album_id: album_id,
-			number: metadata.disc,
-			timestamp_ms: null
+			number: metadata.disc
 		});
 		let track_id = makeBinaryId("track", disc_id, `${metadata.track}`);
 		await stores.tracks.update(queue, {
@@ -320,8 +316,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 			disc_id: disc_id,
 			title: metadata.title,
 			number: metadata.track,
-			copyright: metadata.copyright ?? null,
-			timestamp_ms: null
+			copyright: metadata.copyright ?? null
 		});
 		for (let file_id of file_ids) {
 			await stores.track_files.insert(queue, {
@@ -354,8 +349,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 		await stores.albums.update(queue, {
 			album_id: album_id,
 			title: metadata.title,
-			year_id: year_id ?? null,
-			timestamp_ms: null
+			year_id: year_id ?? null
 		});
 		for (let [index, artist] of metadata.artists.entries()) {
 			let artist_id = makeBinaryId("artist", artist);
@@ -373,8 +367,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 		await stores.discs.update(queue, {
 			disc_id: disc_id,
 			album_id: album_id,
-			number: metadata.disc,
-			timestamp_ms: null
+			number: metadata.disc
 		});
 		if (metadata.tracks.length === file_ids.length) {
 			for (let [index, file_id] of file_ids.entries()) {
@@ -385,8 +378,7 @@ async function indexMetadata(queue: WritableQueue, probe: probes.schema.Probe, .
 					disc_id: disc_id,
 					title: track.title,
 					number: index + 1,
-					copyright: track.copyright ?? metadata.copyright ?? null,
-					timestamp_ms: null
+					copyright: track.copyright ?? metadata.copyright ?? null
 				});
 				await stores.track_files.insert(queue, {
 					track_id: track_id,
@@ -514,7 +506,10 @@ async function indexFile(queue: WritableQueue, file: File): Promise<void> {
 };
 
 async function indexFiles(queue: WritableQueue): Promise<void> {
-	for (let file of await stores.files.filter(queue)) {
+	console.log(`Indexing files...`);
+	let files = await stores.files.filter(queue);
+	console.log(`Database contains ${files.length} files.`);
+	for (let file of files) {
 		if (is.absent(file.index_timestamp)) {
 			console.log(`Indexing ${file.name}...`);
 			await indexFile(queue, file);
@@ -549,7 +544,10 @@ async function getSiblingFiles(queue: ReadableQueue, subject: File): Promise<Arr
 };
 
 async function associateMetadata(queue: WritableQueue): Promise<void> {
-	for (let metadata_file of await stores.metadata_files.filter(queue)) {
+	console.log(`Associating metadata files...`);
+	let metadata_files = await stores.metadata_files.filter(queue);
+	console.log(`Database contains ${metadata_files.length} metadata files.`);
+	for (let metadata_file of metadata_files) {
 		let file = await stores.files.lookup(queue, metadata_file);
 		let path = await getFilePath(queue, file);
 		let fd = libfs.openSync(path.join("/"), "r");
@@ -561,50 +559,65 @@ async function associateMetadata(queue: WritableQueue): Promise<void> {
 };
 
 async function associateImages(queue: WritableQueue): Promise<void> {
-	for (let image_file of await stores.image_files.filter(queue)) {
+	console.log(`Associating image files...`);
+	let image_files = await stores.image_files.filter(queue);
+	console.log(`Database contains ${image_files.length} image files.`);
+	for (let image_file of image_files) {
 		let file = await stores.files.lookup(queue, image_file);
 		let siblings = await getSiblingFiles(queue, file);
 		for (let sibling of siblings) {
 			let track_files = (await links.file_track_files.filter(queue, sibling))
 				.filter((track_file) => track_file.file_id !== image_file.file_id);
-			for (let track_file of track_files) {
-				try {
-					let track = await stores.tracks.lookup(queue, track_file);
-					let disc = await stores.discs.lookup(queue, track);
-					let album = await stores.albums.lookup(queue, disc);
-					await stores.album_files.insert(queue, {
-						album_id: album.album_id,
+			if (track_files.length > 0) {
+				for (let track_file of track_files) {
+					try {
+						let track = await stores.tracks.lookup(queue, track_file);
+						let disc = await stores.discs.lookup(queue, track);
+						let album = await stores.albums.lookup(queue, disc);
+						await stores.album_files.insert(queue, {
+							album_id: album.album_id,
+							file_id: image_file.file_id
+						});
+					} catch (error) {}
+				}
+				continue;
+			}
+			let movie_files = (await links.file_movie_files.filter(queue, sibling))
+				.filter((movie_file) => movie_file.file_id !== image_file.file_id);
+			if (movie_files.length > 0) {
+				for (let movie_file of movie_files) {
+					await stores.movie_files.insert(queue, {
+						movie_id: movie_file.movie_id,
 						file_id: image_file.file_id
 					});
-				} catch (error) {}
-			}
-			let movies = (await links.file_movie_files.filter(queue, sibling))
-				.filter((movie_file) => movie_file.file_id !== image_file.file_id);
-			for (let movie of movies) {
-				await stores.movie_files.insert(queue, {
-					movie_id: movie.movie_id,
-					file_id: image_file.file_id
-				});
+				}
+				continue;
 			}
 			let episode_files = (await links.file_episode_files.filter(queue, sibling))
 				.filter((episode_file) => episode_file.file_id !== image_file.file_id);
-			for (let episode_file of episode_files) {
-				try {
-					let episode = await stores.episodes.lookup(queue, episode_file);
-					let season = await stores.seasons.lookup(queue, episode);
-					let show = await stores.shows.lookup(queue, season);
-					await stores.show_files.insert(queue, {
-						show_id: show.show_id,
-						file_id: image_file.file_id
-					});
-				} catch (error) {}
+			if (episode_files.length > 0) {
+				for (let episode_file of episode_files) {
+					try {
+						let episode = await stores.episodes.lookup(queue, episode_file);
+						let season = await stores.seasons.lookup(queue, episode);
+						let show = await stores.shows.lookup(queue, season);
+						await stores.show_files.insert(queue, {
+							show_id: show.show_id,
+							file_id: image_file.file_id
+						});
+					} catch (error) {}
+				}
+				continue;
 			}
 		}
 	}
 };
 
 async function associateSubtitles(queue: WritableQueue): Promise<void> {
-	for (let subtitle_file of await stores.subtitle_files.filter(queue)) {
+	console.log(`Associating subtitle files...`);
+	let subtitle_files = await stores.subtitle_files.filter(queue);
+	console.log(`Database contains ${subtitle_files.length} subtitle files.`);
+	for (let subtitle_file of subtitle_files) {
 		let file = await stores.files.lookup(queue, subtitle_file);
 		let basename = file.name.split(".")[0];
 		let siblings = (await getSiblingFiles(queue, file))
@@ -619,24 +632,28 @@ async function associateSubtitles(queue: WritableQueue): Promise<void> {
 };
 
 async function removeBrokenEntities(queue: WritableQueue): Promise<void> {
+	console.log(`Removing tracks without files...`);
 	for (let track of await stores.tracks.filter(queue)) {
 		let track_files = await links.track_track_files.filter(queue, track);
 		if (track_files.length === 0) {
 			await stores.tracks.remove(queue, track);
 		}
 	}
+	console.log(`Removing discs without tracks...`);
 	for (let disc of await stores.discs.filter(queue)) {
 		let tracks = await links.disc_tracks.filter(queue, disc);
 		if (tracks.length === 0) {
 			await stores.discs.remove(queue, disc);
 		}
 	}
+	console.log(`Removing albums without discs...`);
 	for (let album of await stores.albums.filter(queue)) {
 		let discs = await links.album_discs.filter(queue, album);
 		if (discs.length === 0) {
 			await stores.albums.remove(queue, album);
 		}
 	}
+	console.log(`Removing artists without albums and tracks...`);
 	for (let artist of await stores.artists.filter(queue)) {
 		let album_artists = await links.artist_album_artists.filter(queue, artist);
 		let track_artists = await links.artist_track_artists.filter(queue, artist);
@@ -644,30 +661,35 @@ async function removeBrokenEntities(queue: WritableQueue): Promise<void> {
 			await stores.artists.remove(queue, artist);
 		}
 	}
+	console.log(`Removing movies without files...`);
 	for (let movie of await stores.movies.filter(queue)) {
 		let movie_files = await links.movie_movie_files.filter(queue, movie);
 		if (movie_files.length === 0) {
 			await stores.movies.remove(queue, movie);
 		}
 	}
+	console.log(`Removing episodes without files...`);
 	for (let episode of await stores.episodes.filter(queue)) {
 		let episode_files = await links.episode_episode_files.filter(queue, episode);
 		if (episode_files.length === 0) {
 			await stores.episodes.remove(queue, episode);
 		}
 	}
+	console.log(`Removing seasons without episodes...`);
 	for (let season of await stores.seasons.filter(queue)) {
 		let episodes = await links.season_episodes.filter(queue, season);
 		if (episodes.length === 0) {
 			await stores.seasons.remove(queue, season);
 		}
 	}
+	console.log(`Removing shows without seasons...`);
 	for (let show of await stores.shows.filter(queue)) {
 		let seasons = await links.show_seasons.filter(queue, show);
 		if (seasons.length === 0) {
 			await stores.shows.remove(queue, show);
 		}
 	}
+	console.log(`Removing actors without movies and shows...`);
 	for (let actor of await stores.actors.filter(queue)) {
 		let movie_actors = await links.actor_movie_actors.filter(queue, actor);
 		let show_actors = await links.actor_show_actors.filter(queue, actor);
@@ -675,6 +697,7 @@ async function removeBrokenEntities(queue: WritableQueue): Promise<void> {
 			await stores.actors.remove(queue, actor);
 		}
 	}
+	console.log(`Removing genres without movies and shows...`);
 	for (let genre of await stores.genres.filter(queue)) {
 		let movie_genres = await links.genre_movie_genres.filter(queue, genre);
 		let show_genres = await links.genre_show_genres.filter(queue, genre);
@@ -682,6 +705,7 @@ async function removeBrokenEntities(queue: WritableQueue): Promise<void> {
 			await stores.genres.remove(queue, genre);
 		}
 	}
+	console.log(`Removing years without albums, movies and episodes...`);
 	for (let year of await stores.years.filter(queue)) {
 		let albums = await links.year_albums.filter(queue, year);
 		let movies = await links.year_movies.filter(queue, year);
@@ -830,6 +854,7 @@ export async function computeMovieSuggestions(queue: WritableQueue): Promise<voi
 };
 
 export async function computeDerivedValues(queue: WritableQueue): Promise<void> {
+	console.log(`Computing derived values...`);
 	await computeAlbumTimestamps(queue);
 	await computeMovieTimestamps(queue);
 	await computeShowTimestamps(queue);
@@ -838,7 +863,7 @@ export async function computeDerivedValues(queue: WritableQueue): Promise<void> 
 
 export async function migrateLegacyData(queue: WritableQueue): Promise<void> {
 	if (libfs.existsSync(TABLES_ROOT.join("/"))) {
-		console.log(`Migrating legacy user data...`);
+		console.log(`Migrating legacy data...`);
 		let users = loadTable("users", schema.User, (record) => record.user_id);
 		for (let user of users) {
 			try {
@@ -947,25 +972,24 @@ export async function runIndexer(): Promise<void> {
 		await visitDirectory(queue, config.media_path, null);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
-		console.log(`Indexing files...`);
 		await indexFiles(queue);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
-		console.log(`Associating...`);
 		await associateMetadata(queue);
+	});
+	await transactionManager.enqueueWritableTransaction(async (queue) => {
 		await associateImages(queue);
+	});
+	await transactionManager.enqueueWritableTransaction(async (queue) => {
 		await associateSubtitles(queue);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
-		console.log(`Removing broken entities...`);
 		await removeBrokenEntities(queue);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
-		console.log(`Computing derived values...`);
 		await computeDerivedValues(queue);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
-		console.log(`Migrating legacy data...`);
 		await migrateLegacyData(queue);
 	});
 	await transactionManager.enqueueWritableTransaction(async (queue) => {
