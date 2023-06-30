8e75a266e2b1b513361715331440cb381a1fdb0d
Joel Ek <contact@joelek.se>
2023-06-30 20:20:21 +0200

    Improve binary file reader.

--- source/database/probes/readers.ts
+++ source/database/probes/readers.ts
@@ -2,32 +2,41 @@ import * as libfs from "fs";
 
 export class Binary {
 	private fd: number;
-	private offset: number;
+	private cursor: number;
+	private size: number;
 
 	private read(buffer: Buffer): Buffer {
-		let bytes = libfs.readSync(this.fd, buffer, 0, buffer.length, this.offset);
+		let bytes = libfs.readSync(this.fd, buffer, 0, buffer.length, this.cursor);
 		if (bytes !== buffer.length) {
 			throw `Expected to read ${buffer.length} bytes but read ${bytes}!`;
 		}
-		this.offset += bytes;
+		this.cursor += bytes;
 		return buffer;
 	}
 
 	private skip(length: number): void {
-		this.offset += length;
+		this.cursor += length;
+	}
+
+	private tell(): { cursor: number, size: number } {
+		return {
+			cursor: this.cursor,
+			size: this.size
+		};
 	}
 
 	constructor(fd: number) {
 		this.fd = fd;
-		this.offset = 0;
+		this.cursor = 0;
+		this.size = libfs.fstatSync(fd).size;
 	}
 
-	newContext<A>(context: (read: (buffer: Buffer) => Buffer, skip: (length: number) => void) => A): A {
-		let offset = this.offset;
+	newContext<A>(context: (read: (buffer: Buffer) => Buffer, skip: (length: number) => void, tell: () => { cursor: number, size: number }) => A): A {
+		let cursor = this.cursor;
 		try {
-			return context((buffer) => this.read(buffer), (length) => this.skip(length));
+			return context((buffer) => this.read(buffer), (length) => this.skip(length), () => this.tell());
 		} catch (error) {
-			this.offset = offset;
+			this.cursor = cursor;
 			throw error;
 		}
 	}
