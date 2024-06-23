207cf6f4248957061162d9e4985c923c22e905f6
Joel Ek <contact@joelek.se>
2024-06-23 22:30:05 +0200

    Save comments.

--- source/database/atlas.ts
+++ source/database/atlas.ts
@@ -1316,6 +1316,8 @@ export const transactionManager = context.createTransactionManager("./private/db
 	getArtistTracksByAffinity
 });
 
+// fs.writeFileSync("stats.json", JSON.stringify(transactionManager.getStatistics(), null, "\t"));
+
 export const stores = transactionManager.stores;
 export const links = transactionManager.links;
 export const queries = transactionManager.queries;
